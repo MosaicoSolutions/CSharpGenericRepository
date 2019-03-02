784664482a34e56f0f81775117be11fbcbf7417d
Bruno X. Moura <bruno.xavier.moura@gmail.com>
2019-03-02 09:52:05 -0300

    using virtual modifier

--- src/MosaicoSolutions.GenericRepository/Repositories/Read/Interfaces/ReadRepository.cs
+++ src/MosaicoSolutions.GenericRepository/Repositories/Read/Interfaces/ReadRepository.cs
@@ -20,39 +20,39 @@ namespace MosaicoSolutions.GenericRepository.Repositories.Read.Interfaces
             DbSet = dbContext.Set<TEntity>();
         }
 
-        public bool Exists(params object[] keyValues)
+        public virtual bool Exists(params object[] keyValues)
         {
             var entity = DbSet.Find(keyValues);
             return entity != null;
         }
 
-        public Task<bool> ExistsAsync(object[] keyValues, CancellationToken cancellationToken = default(CancellationToken))
+        public virtual Task<bool> ExistsAsync(object[] keyValues, CancellationToken cancellationToken = default(CancellationToken))
             => DbSet.FindAsync(keyValues, cancellationToken).ContinueWith(task => task.Result != null);
 
-        public Task<bool> ExistsAsync(params object[] keyValues)
+        public virtual Task<bool> ExistsAsync(params object[] keyValues)
             => DbSet.FindAsync(keyValues).ContinueWith(task => task.Result != null);
 
-        public TEntity FindById(params object[] keyValues)
+        public virtual TEntity FindById(params object[] keyValues)
             => DbSet.Find(keyValues);    
         
-        public Task<TEntity> FindByIdAsync(params object[] keyValues)
+        public virtual Task<TEntity> FindByIdAsync(params object[] keyValues)
             => DbSet.FindAsync(keyValues); 
 
-        public Task<TEntity> FindByIdAsync(object[] keyValues, CancellationToken cancellationToken = default(CancellationToken))
+        public virtual Task<TEntity> FindByIdAsync(object[] keyValues, CancellationToken cancellationToken = default(CancellationToken))
             => DbSet.FindAsync(keyValues, cancellationToken);
 
-        public List<TEntity> Find(QueryOptions<TEntity> queryOptions)
+        public virtual List<TEntity> Find(QueryOptions<TEntity> queryOptions)
         {
             var query = Query(queryOptions);
             return query.ToList();
         }
 
-        public Task<List<TEntity>> FindAsync(QueryOptions<TEntity> queryOptions)
+        public virtual Task<List<TEntity>> FindAsync(QueryOptions<TEntity> queryOptions)
         {
             var query = Query(queryOptions);
             return query.ToListAsync();
         }
 
-        public IQuery<TEntity> Query(QueryOptions<TEntity> queryOptions) => new Query<TEntity>(DbSet, queryOptions);
+        public virtual IQuery<TEntity> Query(QueryOptions<TEntity> queryOptions) => new Query<TEntity>(DbSet, queryOptions);
     }
 }
