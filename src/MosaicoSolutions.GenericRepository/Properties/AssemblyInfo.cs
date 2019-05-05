02b81464c72698f3cec508f51568ccca00603e99
Bruno X. Moura <bruno.xavier.moura@gmail.com>
2019-05-05 12:00:57 -0300

    adding public key token.

--- src/MosaicoSolutions.GenericRepository/Properties/AssemblyInfo.cs
+++ src/MosaicoSolutions.GenericRepository/Properties/AssemblyInfo.cs
@@ -19,6 +19,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MosaicoSolutions.GenericRepository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MosaicoSolutions.GenericRepository")]
+[assembly: System.Reflection.AssemblyKeyFileAttribute("MosaicoSolutions.GenericRepositoryKey.snk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
